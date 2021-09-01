767757cf346d0edf17912c7cf932276f0d4f0b1d
Mohammad Javad Alaeddini <mohammadjavada78@gmail.com>
2021-09-01 11:52:03 +0430

    use SDK

---
+++ service/contentful.js
@@ -0,0 +1,7 @@
+import * as SDK from 'contentful'
+
+const contentful =SDK.createClient({
+    space: '9j9agx4ljwu0',
+    accessToken:'icNXPwqSSL3fAeqFx-47pb9NZcGMo5jJQK_JPlBtKoc'
+})
+export default contentful
